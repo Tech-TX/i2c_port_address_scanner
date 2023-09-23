512332a8aefc1a43ef8710f490d9ac364a81eec5
StanJ <53401742+Tech-TX@users.noreply.github.com>
2023-09-23 10:29:40 -0500

    Update i2c_port_address_scanner.ino
    added Serial.swap() if you've run out of pins

--- i2c_port_address_scanner/i2c_port_address_scanner.ino
+++ i2c_port_address_scanner/i2c_port_address_scanner.ino
@@ -1,23 +1,32 @@
 /* 
-* i2c_port_address_scanner
-* Scans ports D0 to D7 on an ESP8266 and searches for I2C device. based on the original code
+* i2c_port_address_scanner (fixed and expanded)
+* Scans ports D0 to D8 on an ESP8266 and searches for I2C device. based on the original code
 * available on Arduino.cc and later improved by user Krodal and Nick Gammon (www.gammon.com.au/forum/?id=10896)
-* D8 throws exceptions thus it has been left out
-*
+* D8/GPIO15 will fail because of the pulldown resistor, thus it has been left out.
+* Additionally, the original scanner used addresses 1-7 and 120-127 which are reserved addresses.
+* https://www.instructables.com/ESP8266-I2C-PORT-and-Address-Scanner/
+* https://github.com/jainrk/i2c_port_address_scanner
+* 
+* Serial uses UART0, which is mapped to pins GPIO1 (TX) and GPIO3 (RX). If you need to use GPIO1 or GPIO3 for 
+* I2C then Serial may be remapped to GPIO15 (TX) and GPIO13 (RX) by calling Serial.swap() after Serial.begin.
+* Calling Serial.swap() again maps UART0 back to GPIO1 and GPIO3.
 */
 
 #include <Wire.h>
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);             // Leonardo: wait for serial monitor
+//  Serial.swap();  
   Serial.println("\n\nI2C Scanner to scan for devices on each port pair D0 to D7");
   scanPorts();
 }
 
 uint8_t portArray[] = {16, 5, 4, 0, 2, 14, 12, 13};
-//String portMap[] = {"D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7"}; //for Wemos
+//uint8_t portArray[] = {16, 5, 4, 0, 1, 2, 3, 14, 12}; //if using Serial.swap()
+
+//String portMap[] = {"D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7"}; //for D1 Mini
 String portMap[] = {"GPIO16", "GPIO5", "GPIO4", "GPIO0", "GPIO2", "GPIO14", "GPIO12", "GPIO13"};
+//String portMap[] = {"GPIO16", "GPIO5", "GPIO4", "GPIO0", "GPIO1", "GPIO2", "GPIO3", "GPIO14", "GPIO12"}; //if using Serial.swap()
 
 void scanPorts() { 
   for (uint8_t i = 0; i < sizeof(portArray); i++) {
@@ -35,13 +44,14 @@ void check_if_exist_I2C() {
   byte error, address;
   int nDevices;
   nDevices = 0;
-  for (address = 1; address < 127; address++ )  {
+  for (address = 8; address < 120; address++ )  {
     // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
     // a device did acknowledge to the address.
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
-
+    delayMicroseconds(100);
+	
     if (error == 0){
       Serial.print("I2C device found at address 0x");
       if (address < 16)
@@ -51,7 +61,7 @@ void check_if_exist_I2C() {
 
       nDevices++;
     } else if (error == 4) {
-      Serial.print("Unknow error at address 0x");
+      Serial.print("Unknown error at address 0x");
       if (address < 16)
         Serial.print("0");
       Serial.println(address, HEX);
@@ -61,9 +71,8 @@ void check_if_exist_I2C() {
     Serial.println("No I2C devices found");
   else
     Serial.println("**********************************\n");
-  //delay(1000);           // wait 1 seconds for next scan, did not find it necessary
+//  delay(1000);           // wait 1 seconds for next scan
 }
 
 void loop() {
 }
-
